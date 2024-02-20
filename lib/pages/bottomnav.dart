1f208a4aa5cd5fd30ecf21337684464975be18d8
SUJAN <123sujanpr@gmail.com>
2024-02-21 05:21:10 +0545

    food ordering app using flutter

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,113 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:foodordering/pages/profile.dart';
+import 'package:foodordering/pages/wallet.dart';
+
+import 'home.dart';
+import 'order.dart';
+
+// class BotonNav extends StatefulWidget {
+//   const BotonNav({super.key});
+
+//   @override
+//   State<BotonNav> createState() => _BotonNavState();
+// }
+
+// class _BotonNavState extends State<BotonNav> {
+//   int currentTabIndex = 0;
+
+//   late List<Widget> pages;
+//   late Widget currentPage;
+//   late Home homepage;
+//   late Profile profile;
+//   late Order order;
+//   late Wallet wallet;
+
+//   @override
+//   void initState() {
+//     homepage = Home();
+//     order = Order();
+//     profile = Profile();
+//     wallet = Wallet();
+//     pages = [homepage, order, wallet, profile];
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: CupertinoSliverNavigationBar(
+//         items: [
+//         Icon(
+//           Icons.home_outlined,
+//           color: Colors.white,
+//         )
+//       ]),
+//     );
+//   }
+// }
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int currentTabIndex = 0;
+
+  late List<Widget> pages;
+  late Widget currentPage;
+  late Home homepage;
+  late Profile profile;
+  late Order order;
+  late Wallet wallet;
+
+  @override
+  void initState() {
+    homepage = Home();
+    order = Order();
+    profile = Profile();
+    wallet = Wallet();
+    pages = [homepage, order, wallet, profile];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+          height: 65,
+          backgroundColor: Colors.white,
+          color: Colors.black,
+          animationDuration: Duration(milliseconds: 500),
+          onTap: (int index) {
+            setState(() {
+              currentTabIndex = index;
+            });
+          },
+          // bottomNavigationBar:CurvedNavigationBar(items: items)
+          items: [
+            Icon(
+              Icons.home_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.wallet_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.person_outline,
+              color: Colors.white,
+            )
+          ]),
+      body: pages[currentTabIndex],
+    );
+  }
+}
---
+++ lib/pages/order.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Order extends StatefulWidget {
+  const Order({super.key});
+
+  @override
+  State<Order> createState() => OorderState();
+}
+
+class OorderState extends State<Order> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
